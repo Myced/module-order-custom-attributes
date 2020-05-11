4fb82d392b13f2a0bdc9ca3069651e7392fc1964
Ndi Cedric Tifuh <tncedric@gmail.com>
2020-05-11 18:20:33 +0100

    Added custom additions for glotelho

---
+++ Plugin/Sales/Model/OrderGet.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Ecomteck\OrderCustomAttributes\Plugin\Sales\Model;
+
+use Magento\Sales\Api\Data\OrderExtensionFactory;
+use Magento\Sales\Api\Data\OrderExtensionInterface;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\Data\OrderSearchResultInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+
+/**
+ * Class OrderRepositoryPlugin
+ */
+class OrderGet
+{
+    /**
+     * Order feedback field name
+     */
+    const FIELD_NAME = 'ecomteck_order_custom_attributes';
+
+    /**
+     * Order Extension Attributes Factory
+     *
+     * @var OrderExtensionFactory
+     */
+    protected $extensionFactory;
+
+    /**
+     * OrderRepositoryPlugin constructor
+     *
+     * @param OrderExtensionFactory $extensionFactory
+     */
+    public function __construct(OrderExtensionFactory $extensionFactory)
+    {
+        $this->extensionFactory = $extensionFactory;
+    }
+
+    /**
+     * Add "customer_feedback" extension attribute to order data object to make it accessible in API data
+     *
+     * @param OrderRepositoryInterface $subject
+     * @param OrderInterface $order
+     *
+     * @return OrderInterface
+     */
+    public function afterGet(OrderRepositoryInterface $subject, OrderInterface $order)
+    {
+        $sellerInfo = $order->getData(self::FIELD_NAME);
+        $extensionAttributes = $order->getExtensionAttributes();
+        $extensionAttributes = $extensionAttributes ? $extensionAttributes : $this->extensionFactory->create();
+        $extensionAttributes->setSellerInfo($sellerInfo);
+        $order->setExtensionAttributes($extensionAttributes);
+
+        return $order;
+    }
+
+    /**
+     * Add "customer_feedback" extension attribute to order data object to make it accessible in API data
+     *
+     * @param OrderRepositoryInterface $subject
+     * @param OrderSearchResultInterface $searchResult
+     *
+     * @return OrderSearchResultInterface
+     */
+    public function afterGetList(OrderRepositoryInterface $subject, OrderSearchResultInterface $searchResult)
+    {
+        $orders = $searchResult->getItems();
+
+        foreach ($orders as &$order) {
+            $sellerInfo = $order->getData(self::FIELD_NAME);
+            $extensionAttributes = $order->getExtensionAttributes();
+            $extensionAttributes = $extensionAttributes ? $extensionAttributes : $this->extensionFactory->create();
+            $extensionAttributes->setSellerInfo($sellerInfo);
+            $order->setExtensionAttributes($extensionAttributes);
+        }
+
+        return $searchResult;
+    }
+}
